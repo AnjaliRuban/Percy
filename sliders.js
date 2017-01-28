bfa711d590560d4700d8fbac285743c0443a49a9
raymundaden <raymundaden@yahoo.com>
2017-01-28 15:12:57 -0800

    Add files via upload
    here are the sliders!

---
+++ sliders.js
@@ -0,0 +1,12 @@
+function showValue(newValue)
+{
+	document.getElementById("energy").innerHTML=newValue;
+}
+function showValue1(newValue1)
+{
+	document.getElementById("water").innerHTML=newValue1;
+}
+function showValue2(newValue2)
+{
+	document.getElementById("waste").innerHTML=newValue2;
+}
